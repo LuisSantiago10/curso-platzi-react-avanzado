762dc0c6e6a67b7b02e69a2a3350ec2d7ccba831
Luis Santiago <luis@example.com>
2023-04-25 10:08:00 -0600

    create hooks localstrorage and near screen

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,33 +1,17 @@
-import React, { Fragment, useEffect, useRef, useState } from 'react';
+import React, { Fragment } from 'react';
 import { ImgWrapper,Img,Button,Article } from './styles';
-
-
-import { MdFavoriteBorder } from "react-icons/md";
+import { MdFavoriteBorder,MdFavorite } from "react-icons/md";
+import { useLocalStorage } from '../../hook/useLocalStorage';
+import { useNearScreen } from '../../hook/useNearScreen';
 
 const DEFAULT_IMAGE = 'https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png';
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const element = useRef(null)
-  const [show, setShow] = useState(false)
-
-  useEffect(function () {
-
-    Promise.resolve(
-      typeof window.IntersectionObserver !== 'undefined'
-        ? window.IntersectionObserver
-        : import('intersection-observer')
-    ).then(()=>{
-        const observer = new window.IntersectionObserver(function (entries) {
-          const { isIntersecting } = entries[0]
-          console.log(isIntersecting)
-          if (isIntersecting) {
-            setShow(true)
-            observer.disconnect()
-          }
-        })
-        observer.observe(element.current)
-      })
-  }, [element])
+  
+  const [show,element] = useNearScreen();
+  const key = `like-${id}`
+  const [liked,setLiked ] = useLocalStorage(key,false);
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
 
   return (
     <Article ref={element}>
@@ -39,8 +23,8 @@ export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
             </ImgWrapper>
           </a>
 
-          <Button>
-            <MdFavoriteBorder size='32px' /> {likes} likes!
+          <Button onClick={() => setLiked(!liked)}>
+            <Icon size='32px' /> {likes} likes!
           </Button>
         </Fragment>
       }
